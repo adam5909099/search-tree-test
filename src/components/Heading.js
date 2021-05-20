ef2ff9e8752ffd033683d215c646cf088edcc291
adam5909099 <adam5909099@protonmail.com>
2021-05-20 11:33:33 -0400

    Remove legacy highlight code

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,20 +2,6 @@ import React from 'react';
 import Highlighted from './Highlighted';
 
 export default function Heading({ heading, searchText }) {
-  const getHighlightedTitle = () =>
-    heading.title.split(new RegExp(`(${searchText})`), 'gi').map((part, i) => (
-      <span
-        key={i}
-        className={
-          part.toLowerCase() === searchText.toLowerCase() ? 'bg-yellow' : ''
-        }
-      >
-        {part}
-      </span>
-    ));
-
-  console.log({ searchText, highlight: getHighlightedTitle() });
-
   return (
     <span>
       <a href={heading.url}>
